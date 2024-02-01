41051cbfc3269dbe144205127ef78c0d92e9cb42
eVoida <136472260+eVoida@users.noreply.github.com>
2024-02-01 10:53:38 +0530

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -204,7 +204,7 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
         elseif class == "Egg" and unitGems <= 600000 then
             coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
             return
-        elseif (string.find(item, "Key") and not string.find(item, "Lower") or string.find(item, "Ticket")) and unitGems <= 2500 then
+        elseif ((string.find(item, "Key") and not string.find(item, "Lower") or string.find(item, "Ticket")) and unitGems <= 2500 then
             coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
             return
         -----------Enchants
