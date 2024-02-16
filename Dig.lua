2e0b864e3cb170b2010ccd802c804cf89bdf88d6
eVoida <136472260+eVoida@users.noreply.github.com>
2024-02-16 20:22:04 +0530

    Update Dig.lua

--- Dig.lua
+++ Dig.lua
@@ -1,11 +1,10 @@
-local Things = workspace:WaitForChild("__THINGS")
+local Things = Workspace:WaitForChild("__THINGS")
 local Active = Things.__INSTANCE_CONTAINER:WaitForChild("Active")
 local Player = game.Players.LocalPlayer
 local character = Player.Character
 local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
-local Active = Things.__INSTANCE_CONTAINER.Active
-local ActiveBlocks = Active.AdvancedDigsite.Important.ActiveBlocks
-local ActiveChests = Active.AdvancedDigsite.Important.ActiveChests
+local ActiveBlocks = workspace.__THINGS.__INSTANCE_CONTAINER.Active.AdvancedDigsite.Important.ActiveBlocks
+local ActiveChests = workspace.__THINGS.__INSTANCE_CONTAINER.Active.AdvancedDigsite.Important.ActiveChests
 
 local CurrentActive = function()
     return Active:GetChildren()[1]
@@ -18,43 +17,21 @@ local GetChest = function()
     return nil
 end
  
-local GetBlock = function()
-    local blocks = ActiveBlocks:GetChildren()
-    for i, v in pairs(ActiveBlocks:GetChildren()) do
-        local CurrentBlock = blocks[#blocks - i + 1]
-        if CurrentBlock.Color.R > 0.067 or CurrentBlock.Color.G > 0.067 or CurrentBlock.Color.B > 0.067 then
-            local coord = CurrentBlock:GetAttribute('Coord')
-            if (coord.X > 1 and coord.X < 16 and coord.Z > 1 and coord.Z < 16) then
-                return CurrentBlock
-            end
-        end
-    end
-    return nil
-end
- 
 local Dig = function () -- yes Dig my ass out pls mmm daddy
     local Chest = GetChest()
     if Chest then
-        j = 1
-        while Chest.Parent == ActiveChests and j < 20 do
-            humanoidRootPart.CFrame = Chest:FindFirstChildWhichIsA("BasePart").CFrame
-            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer(CurrentActive().Name, "DigChest", Chest:GetAttribute('Coord'))
-            task.wait(0.2)
-        end
-        if j == 20 then
-            Chest.Parent = nil
-        end
+        humanoidRootPart.CFrame = Chest:FindFirstChildWhichIsA("BasePart").CFrame
+        ReplicatedStorage:WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer(CurrentActive().Name, "DigChest", Chest:GetAttribute('Coord'))
+        task.wait(0.05)
     else
-        local Block = GetBlock()
+        local blocks = ActiveBlocks:GetChildren()
+        local Block = blocks[#blocks]
         j = 1
         while Block.Parent == ActiveBlocks and j < 100 do
             humanoidRootPart.CFrame = Block.CFrame
             game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer(CurrentActive().Name, "DigBlock", Block:GetAttribute('Coord'))
-            task.wait(0.2)
+            task.wait(0.05)
             j = j + 1
-        end
-        if Block.Parent == ActiveBlocks then
-            Block:Destroy()
         end
     end
 end
@@ -81,6 +58,7 @@ local Options = Fluent.Options
 
 do
     local Toggle = Tabs.Digging:AddToggle("DigSite", {Title = "Auto AdvancedDigsite", Default = false})
+    local DefaultVelocity = 0
     Toggle:OnChanged(function()
         Toggle = Options.DigSite.Value
         game:GetService("RunService").Stepped:Connect(function()
@@ -88,6 +66,7 @@ do
                 for i,v in next, Player.Character:GetChildren() do
                     if v:IsA("BasePart") then
                         v.CanCollide = false
+                        DefaultVelocity = v.Velocity
                         v.Velocity = Vector3.new(0,0,0)
                     end
                 end
@@ -95,6 +74,7 @@ do
                 for i,v in next, Player.Character:GetChildren() do
                     if v:IsA("BasePart") then
                         v.CanCollide = true
+                        v.Velocity = DefaultVelocity
                     end
                 end
             end
@@ -103,16 +83,6 @@ do
             while Toggle and wait() do
                 pcall(function()
                     Dig()
-                end)
-            end
-        end)
-        spawn(function()
-            while Toggle and wait() do
-                pcall(function()
-                    for i, v in workspace.__THINGS.Orbs:GetChildren() do
-                        v.Position = humanoidRootPart.Position
-                    end
-                    task.wait(1)
                 end)
             end
         end)
@@ -125,8 +95,8 @@ SaveManager:SetIgnoreIndexes({})
 InterfaceManager:SetFolder("AniHub")
 SaveManager:SetFolder("AniHub/specific-game")
 
-InterfaceManager:BuildInterfaceSection(Tabs.Digging)
-SaveManager:BuildConfigSection(Tabs.Digging)
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
 
 
 Window:SelectTab(1)
